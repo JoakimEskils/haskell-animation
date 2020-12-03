7bed1d1d7959e4ff4fdcd170bec9547a6f7c6599
Joakim Eskils <joakim.eskils@bricco.se>
2020-12-03 18:06:38 +0100

    Simple gloss program

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import Graphics.Gloss
 import Packaget
 
 main :: IO ()
-main = someFunc
+main = display window background drawing
+  where
+    window = InWindow "Nice Window" (200, 200) (0, 0)
+    background = white
+    drawing = Circle 80
