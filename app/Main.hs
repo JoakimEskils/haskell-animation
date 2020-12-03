8d56242cee6ce1b45bcadcd203a030a866e74fe1
Joakim Eskils <joakim.eskils@bricco.se>
2020-12-03 18:37:19 +0100

    Simple animation

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,11 @@ module Main where
 import Data.Complex
 import Graphics.Gloss
 
-converge :: Char -> Complex Double -> Complex Double -> Char
-converge c b a
-  | magnitude a > 3 = c
-  | c == ' ' = c
-  | otherwise = converge (pred c) b (a * a + b)
+windowDisplay :: Display
+windowDisplay = InWindow "Window" (200, 200) (10, 10)
 
 main :: IO ()
-main = putStrLn (unlines $ zoom 200 (-3, -1) (-1, 1))
+main = animate windowDisplay white animationFunc
 
-zoom n (r0, r1) (i0, i1) =
-  map (map q . flip map (linspace n r0 r1) . flip (:+)) (linspace (n / 2) i0 i1)
-  where
-    linspace m a b = [a, a + ((b - a) / m) .. b]
-    q u = converge '~' u u
+animationFunc :: Float -> Picture
+animationFunc time = thickCircle (4 * time) (7 * time)
