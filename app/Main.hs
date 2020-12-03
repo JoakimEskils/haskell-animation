3b6f70be789c5466537efb25d2e09f6a24d50ce6
Joakim Eskils <joakim.eskils@bricco.se>
2020-12-03 18:10:22 +0100

    Growing circle

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,11 @@ module Main where
 import Graphics.Gloss
 import Packaget
 
+windowDisplay :: Display
+windowDisplay = InWindow "Window" (200, 200) (10, 10)
+
+animationFunc :: Float -> Picture
+animationFunc time = Circle (2 * time)
+
 main :: IO ()
-main = display window background drawing
-  where
-    window = InWindow "Nice Window" (200, 200) (0, 0)
-    background = white
-    drawing = Circle 80
+main = animate windowDisplay white animationFunc
