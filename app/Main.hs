b28b2dc9322426b76bf433cfe6f04ce3eb36ad57
Joakim Eskils <joakim.eskils@bricco.se>
2020-12-03 18:24:11 +0100

    Simple ascii mandelbrot

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,19 @@
 module Main where
 
+import Data.Complex
 import Graphics.Gloss
-import Packaget
 
-width, height :: Int
-(width, height) = (10000, round . (* sqrt 2) . fromIntegral $ width)
-
-windowDisplay :: Display
-windowDisplay = InWindow "Mandelbrot" (200, 200) (10, 10)
+converge :: Char -> Complex Double -> Complex Double -> Char
+converge c b a
+  | magnitude a > 3 = c
+  | c == ' ' = c
+  | otherwise = converge (pred c) b (a * a + b)
 
 main :: IO ()
-main = display windowDisplay white (Circle 80)
+main = putStrLn (unlines $ zoom 200 (-3, -1) (-1, 1))
+
+zoom n (r0, r1) (i0, i1) =
+  map (map q . flip map (linspace n r0 r1) . flip (:+)) (linspace (n / 2) i0 i1)
+  where
+    linspace m a b = [a, a + ((b - a) / m) .. b]
+    q u = converge '~' u u
